794466ae67a200eedaf36dcd70cb04eab852f0b5
Loc Nguyen <lnguyen72@huskers.unl.edu>
2023-04-11 17:46:27 -0500

    fix html, css, and mess around with javascript

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * Testing functionality of JavaScript Light/Dark mode toggle!
+ */
+function colorChange() {
+    const colorElement = document.body;
+    colorElement.classList.toggle("dark-mode");
+}
