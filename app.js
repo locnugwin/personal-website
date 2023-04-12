e8a0b2d96e60a45200d461ecea8eff1416407061
Loc Nguyen <lnguyen72@huskers.unl.edu>
2023-04-12 18:59:35 -0500

    css light/dark mode changes and js documentation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /**
  * Testing functionality of JavaScript Light/Dark mode toggle!
+ * Tutorial [here](https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp)
  */
 function colorChange() {
     const colorElement = document.body;
