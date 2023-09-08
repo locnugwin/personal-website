3f1728f623e75d44b1c73f3f8a1b8b15015b72c9
Loc Nguyen <lnguyen72@huskers.unl.edu>
2023-09-08 16:15:43 -0500

    fix css hovering + profile update

---
+++ script.js
@@ -0,0 +1,36 @@
+// Function to toggle between light and dark mode
+function toggleMode() {
+    const colorElement = document.body;
+    colorElement.classList.toggle("light-mode"); // Toggle the "light-mode" class on the body element
+
+    const modeToggle = document.getElementById("modeToggle"); // Get the anchor element with the ID "modeToggle"
+    const button = modeToggle.querySelector("button"); // Get the button element inside the anchor
+
+    if (colorElement.classList.contains("light-mode")) {
+        // If in light mode, change button text to "Toggle Dark Mode" and update link colors
+        button.textContent = "Toggle Dark Mode";
+        updateLinkColors("#171717", "#dbd6cb"); // Update link colors with dark mode colors
+    } else {
+        // If in dark mode, change button text to "Toggle Light Mode" and update link colors
+        button.textContent = "Toggle Light Mode";
+        updateLinkColors("#dbd6cb", "#171717"); // Update link colors with light mode colors
+    }
+}
+
+// Function to update link colors based on mode
+function updateLinkColors(linkColor, hoverColor) {
+    const links = document.querySelectorAll("a"); // Select all links in the document
+
+    links.forEach(link => {
+        link.style.color = linkColor; // Set the link's color
+
+        // Add mouseover and mouseout event listeners for hover effect
+        link.addEventListener("mouseover", function() {
+            this.style.color = hoverColor; // Change color on mouseover
+        });
+
+        link.addEventListener("mouseout", function() {
+            this.style.color = linkColor; // Change back to original color on mouseout
+        });
+    });
+}
